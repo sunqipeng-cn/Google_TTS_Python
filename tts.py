ea568ec9b6060a9278feecb0d10cf912dbccf38f
Sandtears <me@sandtears.com>
2013-10-02 22:15:09 +0800

    兼容带空格的参数

--- tts.py
+++ tts.py
@@ -19,16 +19,17 @@ def tts(str):
     s = requests.Session()
     s.get("http://translate.google.com/#zh-CN/en/" + urllib.quote(str))
     res = s.get("http://translate.google.com/translate_tts?ie=UTF-8&q=" + urllib.quote(str) + "&tl=zh-CN&total=1&idx=0&textlen=8&prev=input").content
-    f = open("tts-" + str + ".mpeg", "w")
+    f = open("tts-temp.mpeg", "w")
     f.write(res)
     f.close()
 
 def say(str):
     tts(str)
     import os
-    os.system("mplayer tts-" + str + ".mpeg")
-    os.system("rm -f tts-" + str + ".mpeg")
+    os.system("mplayer tts-temp.mpeg")
+    os.system("rm -f tts-temp.mpeg")
 
 if __name__ == "__main__":
-    import sys
-    say(sys.argv[1])
+    import sys, locale
+    str = sys.argv[1].decode(locale.getdefaultlocale()[1]).encode('UTF-8')
+    say(str)
